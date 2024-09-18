5e40413e9ff2bcfe99e435227a4998b1426dc61a
Bruno Maruszczak <72678261+bruno-maruszczak@users.noreply.github.com>
2024-09-18 20:52:44 +0200

    Task 1 and 2 done
    Constant Torque traj_gen broke ??

--- controllers/feedback_linearization_controller.py
+++ controllers/feedback_linearization_controller.py
@@ -4,12 +4,23 @@ from .controller import Controller
 
 
 class FeedbackLinearizationController(Controller):
-    def __init__(self, Tp):
+    def __init__(self, Tp, kp=1.0, kd=0.1):
         self.model = ManiuplatorModel(Tp)
+        self.kp = kp
+        self.kd = kd
+
+
+
 
     def calculate_control(self, x, q_r, q_r_dot, q_r_ddot):
         """
         Please implement the feedback linearization using self.model (which you have to implement also),
         robot state x and desired control v.
         """
-        return NotImplementedError()
+        q = x[:2]
+        q_dot = x[2:]
+        #u = q_r_ddot + self.kd*(q_dot - q_r_dot) + self.kp*(q - q_r)
+        u = q_r_ddot
+
+        v = self.model.M(x) @ u + self.model.C(x) @ q_dot 
+        return v
