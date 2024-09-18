2d3f338ef7673083cde420f8b133b96063344701
Bruno Maruszczak <72678261+bruno-maruszczak@users.noreply.github.com>
2024-09-18 21:33:35 +0200

    Tests on polynomial traj

--- models/manipulator_model.py
+++ models/manipulator_model.py
@@ -4,7 +4,7 @@ import numpy as np
 class ManiuplatorModel: # Xd
     def __init__(self, Tp):
         self.Tp = Tp
-        self.l1 = 0.55
+        self.l1 = 0.5
         self.d1 = self.l1 * 0.5
         self.r1 = 0.04
         self.m1 = 3.
@@ -14,7 +14,7 @@ class ManiuplatorModel: # Xd
         self.m2 = 2.4
         self.I_1 = 1 / 12 * self.m1 * (3 * self.r1 ** 2 + self.l1 ** 2)
         self.I_2 = 1 / 12 * self.m2 * (3 * self.r2 ** 2 + self.l2 ** 2)
-        self.m3 = 0.12
+        self.m3 = 0.1
         self.r3 = 0.05
         self.I_3 = 2. / 5 * self.m3 * self.r3 ** 2
 
